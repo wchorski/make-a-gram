55e92c7e25a8723df58b1235b6f401a7059218b6
wchorski <wchorski@gmail.com>
2023-04-25 15:01:02 -0500

    umami analytics behind next proxy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+const ANALYTICS_URL = process.env.NEXT_PUBLIC_UMAMI_URL + '/:match*'
+console.log('ANALYTICS_URL', ANALYTICS_URL);
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/stts/:match*',
+        destination: ANALYTICS_URL,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import { GlobalStyles } from "../styles/ThemeGlobalStyle" 
+import  Script  from "next/script";
+
+const UMAMI_SCRIPT = process.env.NEXT_PUBLIC_UMAMI_SCRIPT
+const UMAMI_ID = process.env.NEXT_PUBLIC_UMAMI_ID
 
 function MyApp({ Component, pageProps }) {
+
+
   return (<>
+
+    <Script
+      id="umami-next"
+      strategy="afterInteractive"
+      async
+      data-website-id={UMAMI_ID}
+      src={`/stts/${UMAMI_SCRIPT}`}
+    />
+ 
+    
     <GlobalStyles />
     
     <Component {...pageProps} />
